c79a1dd45f78a2cfb77cbb370e96af956d4ae887
Mathieu <math.lahousse@gmail.com>
2015-12-21 23:19:25 +0100

    Create files with express. Add BCrypt and a presave for user model.

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+var userController = require('../controllers/user');
+
+/* GET users listing. */
+router.get('/', userController.getUsers);
+router.post('/', userController.postUsers);
+
+module.exports = router;
